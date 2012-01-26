8cf30880bc86c172cf8962f12f7c729bdd92b606
Igor Laishen <igor.lisx@gmail.com>
2012-01-26 04:35:19 +0200

    New CustomFieldsTable composite component for Jira editor

---
+++ webui/src/main/java/com/taskadapter/webui/CustomField.java
@@ -0,0 +1,33 @@
+package com.taskadapter.webui;
+
+/**
+ * @author Igor Laishen
+ */
+public class CustomField {
+    private String id;
+    private String value;
+
+    public CustomField () {
+    }
+
+    public CustomField (String id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public String getId () {
+        return id;
+    }
+
+    public void setId (String id) {
+        this.id = id;
+    }
+
+    public String getValue () {
+        return value;
+    }
+
+    public void setValue (String value) {
+        this.value = value;
+    }
+}
--- webui/src/main/java/com/taskadapter/webui/HomePage.java
+++ webui/src/main/java/com/taskadapter/webui/HomePage.java
@@ -1,7 +1,11 @@
 package com.taskadapter.webui;
 
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Alexey Skorokhodov
@@ -15,6 +19,49 @@ public class HomePage extends Page {
         VerticalLayout layout = new VerticalLayout();
         layout.setWidth("100%");
         layout.addComponent(new Label("Home page"));
+
+
+/*
+        // For CustomFieldsTable testing
+
+        List<CustomField> customFields = Arrays.asList(
+                new CustomField("0", "Very very very very long value"),
+                new CustomField("1", "Not very long value"),
+                new CustomField("100", "Not very long value"),
+                new CustomField("9999", "Very very very very long value"),
+                new CustomField("15", "just value"),
+                new CustomField("5", "just value"),
+                new CustomField("2", "Short value")
+        );
+        final CustomFieldsTable table = new CustomFieldsTable(customFields);
+
+        final Label label = new Label();
+        label.setContentMode(Label.CONTENT_XHTML);
+
+        Button btn = new Button("test");
+        btn.addListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick (Button.ClickEvent event) {
+                List<CustomField> newCustomFields = table.getCustomFields();
+                if(!newCustomFields.isEmpty()) {
+                    StringBuilder sb = new StringBuilder();
+                    for(CustomField customField : newCustomFields) {
+                        sb.append("id=").append(customField.getId())
+                                .append(" value=").append(customField.getValue())
+                                .append("<br/>");
+                    }
+                    label.setValue(sb.toString());
+                } else {
+                    label.setValue("no custom fields");
+                }
+            }
+        });
+
+        layout.addComponent(table);
+        layout.addComponent(btn);
+        layout.addComponent(label);
+*/
+
         setCompositionRoot(layout);
     }
 
