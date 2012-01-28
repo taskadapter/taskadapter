77765248b050a4facb80e739307bfb11537b8b24
Alexey Skorokhodov <alskor@gmail.com>
2012-01-28 15:31:08 -0800

    reformatted code using the standard IDEA 11 formatter.

--- webui/src/main/java/com/taskadapter/webui/UpdateManager.java
+++ webui/src/main/java/com/taskadapter/webui/UpdateManager.java
@@ -13,53 +13,55 @@ import java.util.Properties;
 
 public class UpdateManager {
 
-  private String lastVersion;
+    private String lastVersion;
 
-  public UpdateManager() {
+    public UpdateManager() {
 // disabled for now to not generate a lot of useless "check version" requests to the server.
 // we'll enable before the release.
 //    loadLastVersion();
-      setHardcodedLastVersionForTesting();
-  }
-
-  private void setHardcodedLastVersionForTesting() {
-      lastVersion = "2.0.0_dev";
-  }
-
-  private void loadLastVersion() {
-    try {
-      Properties properties = new Properties();
-      properties.load(MyIOUtils.getResourceAsStream("bat.properties"));
-      String url = properties.getProperty("update_site_url");
-      DefaultHttpClient httpclient = HttpUtil.getNewHttpClient();
-
-      HttpGet request = new HttpGet(url);
-
-      HttpResponse httpResponse = httpclient.execute(request);
-      HttpEntity responseEntity = httpResponse.getEntity();
-      String responseBody = EntityUtils.toString(responseEntity);
-      this.lastVersion = responseBody.trim();
-    } catch (IOException e) {
-      throw new RuntimeException(e.toString(), e);
+        setHardcodedLastVersionForTesting();
     }
 
-  }
+    private void setHardcodedLastVersionForTesting() {
+        lastVersion = "2.0.0_dev";
+    }
 
-  public String getCurrentVersion() {
-    return "1.0.1";
-  }
+    private void loadLastVersion() {
+        try {
+            Properties properties = new Properties();
+            properties.load(MyIOUtils.getResourceAsStream("bat.properties"));
+            String url = properties.getProperty("update_site_url");
+            DefaultHttpClient httpclient = HttpUtil.getNewHttpClient();
 
-  /**
-   * check the last TA version available for download on the website.
-   *
-   * @return
-   */
-  public String getLatestAvailableVersion() {
-    return lastVersion;
-  }
+            HttpGet request = new HttpGet(url);
 
-  public static boolean isOutdated(String currentVersion, String lastAvailableVersion) {
-    int res = currentVersion.compareTo(lastAvailableVersion);
-    return res < 0;
-  }
+            HttpResponse httpResponse = httpclient.execute(request);
+            HttpEntity responseEntity = httpResponse.getEntity();
+            String responseBody = EntityUtils.toString(responseEntity);
+            this.lastVersion = responseBody.trim();
+        } catch (IOException e) {
+            throw new RuntimeException(e.toString(), e);
+        }
+
+    }
+
+    public String getCurrentVersion() {
+        return "1.0.1";
+    }
+
+    /**
+     * check the last TA version available for download on the website.
+     *
+     * @return
+     */
+    public String getLatestAvailableVersion() {
+        return lastVersion;
+    }
+
+    public static boolean isOutdated(String currentVersion, String lastAvailableVersion) {
+        int res = currentVersion.compareTo(lastAvailableVersion);
+        return res < 0;
+    }
+
+
 }
