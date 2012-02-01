c5faab5b9a5478d0231d13d42a5711f48164d5d0
Maxim Zyukin <m.zyukin@gmail.com>
2012-02-02 04:04:19 +0700

    Added MantisEditor.

--- connectors-editors/connector.mantis.editor/src/main/java/com/taskadapter/connector/mantis/editor/MantisEditor.java
+++ connectors-editors/connector.mantis.editor/src/main/java/com/taskadapter/connector/mantis/editor/MantisEditor.java
@@ -1,7 +1,11 @@
 package com.taskadapter.connector.mantis.editor;
 
 import com.taskadapter.connector.definition.ConnectorConfig;
+import com.taskadapter.connector.mantis.MantisConfig;
+import com.taskadapter.connector.mantis.MantisDescriptor;
 import com.taskadapter.web.configeditor.ConfigEditor;
+import com.taskadapter.web.configeditor.EditorUtil;
+import com.vaadin.ui.CheckBox;
 
 /**
  * @author Alexey Skorokhodov
@@ -10,67 +14,26 @@ public class MantisEditor extends ConfigEditor {
 
     public MantisEditor(ConnectorConfig config) {
         super(config);
+        buildUI();
+        setData(config);
     }
 
-    // TODO implement the panel
-/*
-    	@Override
-    	public ConnectorConfig getResult() throws ValidationGUIException {
-    		MantisConfig config = new MantisConfig();
-
-    		config.setSaveIssueRelations(saveRelations.getSelection());
-    		config.setFindUserByName(findUserByName.getSelection());
-
-    		return config;
-    	}
-
-    	@Override
-    	protected void createComposite() {
-    		Composite composite = new Composite(tabComposite, SWT.NONE);
-    		RowLayout rowLayout = new RowLayout();
-    		rowLayout.type = SWT.VERTICAL;
-    		composite.setLayout(rowLayout);
-
-    		createLabelSection(composite, 4);
-
-    		Composite serverAndProjectGroup = new Composite(composite, SWT.NONE);
-    		final GridLayout gridLayout1 = new GridLayout(2, false);
-    		serverAndProjectGroup.setLayout(gridLayout1);
-
-    		serverPanel = addServerPanel(serverAndProjectGroup);
-
-    		projectPanel = addProjectPanel(this, serverAndProjectGroup, new MantisProjectProcessor(this));
-    		projectPanel.setProjectKeyRequired(true);
-
-    		addFindUsersByNameElement(serverAndProjectGroup);
-
-    		final GridData gridData4col = new GridData();
-    		gridData4col.horizontalSpan = 4;
-    		findUserByName.setLayoutData(gridData4col);
-
-    		addFieldsMappingSection(composite);
-    		addSaveRelationSection(composite);
-    	}
-
-    	@Override
-    	protected void setDataToForm(ConnectorConfig oldConfig) {
-    		MantisConfig config = (MantisConfig) oldConfig;
-
-    		setBooleanIfNotNull(saveRelations,
-    				config.getSaveIssueRelations());
-
-    		serverPanel.setServerInfo(config.getServerInfo());
-    		ProjectInfo projectInfo = new ProjectInfo();
-    		projectInfo.setProjectKey(config.getProjectKey());
-    		projectInfo.setQueryId(config.getQueryId());
-    		projectPanel.setProjectInfo(projectInfo);
-
-    		setBooleanIfNotNull(findUserByName, config.getFindUserByName());
-    	}
-*/
+    private void buildUI() {
+        addServerPanel();
+        addProjectPanel(this, new MantisProjectProcessor(this));
+        addFindUsersByNameElement();
+        addSaveRelationSection();
+        addFieldsMappingPanel(MantisDescriptor.instance.getAvailableFieldsProvider(), config.getFieldsMapping());
+    }
 
     @Override
     public ConnectorConfig getPartialConfig() {
         return config;
     }
+
+    private void addSaveRelationSection() {
+        CheckBox saveRelations = new CheckBox("Save issue relations (follows/precedes)");
+        addComponent(saveRelations);
+    }
+
 }
