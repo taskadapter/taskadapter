d4c9e8e7a6531f51ee4a333b30e0f0b2d85f610c
Maxim Zyukin <m.zyukin@gmail.com>
2012-02-02 04:37:57 +0700

    Added GithubEditor.

--- connectors-editors/connector.github.editor/src/main/java/com/taskadapter/connector/github/GithubEditor.java
+++ connectors-editors/connector.github.editor/src/main/java/com/taskadapter/connector/github/GithubEditor.java
@@ -3,6 +3,7 @@ package com.taskadapter.connector.github;
 import com.taskadapter.connector.definition.ConnectorConfig;
 import com.taskadapter.connector.definition.WebServerInfo;
 import com.taskadapter.web.configeditor.ConfigEditor;
+import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 
@@ -17,38 +18,23 @@ public class GithubEditor extends ConfigEditor {
     public GithubEditor(ConnectorConfig config) {
         super(config);
         buildUI();
-        addFieldsMappingPanel(GithubDescriptor.instance.getAvailableFieldsProvider(), config.getFieldsMapping());
         setData(config);
-        setGitHubData();
+    }
+
+    private void buildUI() {
+        addServerPanel();
+        addProjectPanel(this, new GithubProjectProcessor(this));
+        addSaveRelationSection();
+        addFieldsMappingPanel(GithubDescriptor.instance.getAvailableFieldsProvider(), config.getFieldsMapping());
     }
 
     @Override
     public ConnectorConfig getPartialConfig() {
-        GithubConfig newConfig = new GithubConfig();
-
-        WebServerInfo serverInfo = new WebServerInfo(null, (String) userNameText.getValue(), (String) passwordText.getValue());
-        newConfig.setServerInfo(serverInfo);
-
-        newConfig.setProjectKey((String) projectKey.getValue());
-
-        return newConfig;
+        return config;
     }
 
-    private void buildUI() {
-
-        userNameText = new TextField("Login:");
-        addComponent(userNameText);
-        passwordText = new PasswordField("Password:");
-        addComponent(passwordText);
-        projectKey = new TextField("Project key:");
-        addComponent(projectKey);
-    }
-
-    private void setGitHubData() {
-        GithubConfig githubConfig = (GithubConfig) config;
-        WebServerInfo serverInfo = githubConfig.getServerInfo();
-        setIfNotNull(userNameText, serverInfo.getUserName());
-        setIfNotNull(passwordText, serverInfo.getPassword());
-        setIfNotNull(projectKey, githubConfig.getProjectKey());
+    private void addSaveRelationSection() {
+        CheckBox saveRelations = new CheckBox("Save issue relations (follows/precedes)");
+        addComponent(saveRelations);
     }
 }
