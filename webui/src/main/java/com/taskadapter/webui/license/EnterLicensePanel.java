970b1d545ad40fe221b423edb289d5237f9579c8
Igor Laishen <igor.lisx@gmail.com>
2012-04-18 23:05:38 +0300

    bug-61797: "license text" area is too small. Width value has been moved to styles.css file.

--- webui/src/main/java/com/taskadapter/webui/license/EnterLicensePanel.java
+++ webui/src/main/java/com/taskadapter/webui/license/EnterLicensePanel.java
@@ -19,7 +19,7 @@ public class EnterLicensePanel extends VerticalLayout {
         addComponent(new Label("UNREGISTERED"));
 
         licenseArea = new TextArea("Paste the complete contents of the license file here");
-        licenseArea.setWidth("275px");
+        licenseArea.setStyleName("licenseArea");
 
         addComponent(licenseArea);
 
