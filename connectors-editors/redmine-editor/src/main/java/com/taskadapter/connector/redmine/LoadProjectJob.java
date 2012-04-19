f4640d55111a24e5dc0dc61765b37b6290540392
Alexey Skorokhodov <alskor@gmail.com>
2012-04-18 21:36:15 -0700

    removed dead code

--- connectors-editors/redmine-editor/src/main/java/com/taskadapter/connector/redmine/LoadProjectJob.java
+++ connectors-editors/redmine-editor/src/main/java/com/taskadapter/connector/redmine/LoadProjectJob.java
@@ -15,28 +15,18 @@ public class LoadProjectJob {
 
     public LoadProjectJob(LoadProjectJobResultListener resultsListener,
                           RedmineManager mgr, String projectKey) {
-        System.out.println("Loading project " + projectKey);
         this.resultsListener = resultsListener;
         this.mgr = mgr;
         this.projectKey = projectKey;
     }
 
     public void run() {
-
         try {
             this.project = mgr.getProjectByKey(projectKey);
         } catch (final Exception e) {
             System.out.println(e);
-        } finally {
-//			monitor.done();
         }
-//		Display.getDefault().asyncExec(new Runnable() {
-//			public void run() {
         resultsListener.notifyProjectLoaded(project);
-//			}
-
-//		});
-
     }
 
 }
