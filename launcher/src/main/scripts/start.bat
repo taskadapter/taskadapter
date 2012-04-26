12d3aca513a1cac2a79e71e8250138bd1326ff00
Alexey Skorokhodov <alskor@gmail.com>
2012-04-25 23:23:19 -0700

    added 2 seconds delay before opening browser. this is not a great solution, but...

--- launcher/src/main/scripts/start.bat
+++ launcher/src/main/scripts/start.bat
@@ -1,4 +1,9 @@
 start java -jar launcher.jar --port=8080
 
+@echo ------------------------------------------------
+@echo Waiting 2 seconds for Task Adapter application to start...
+@echo ------------------------------------------------
+sleep 2
+
 @REM open the default web browser
 start http://localhost:8080/ta
