4b5235f44902fb20e1f4863817f423188b598ee1
Alexey Skorokhodov <alskor@gmail.com>
2012-04-26 23:35:54 -0700

    #50058 check if Java 1.6 or newer is installed in TA start script (LINUX script)

--- launcher/src/main/scripts/start.sh
+++ launcher/src/main/scripts/start.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+
+# Task Adapter start script checks if JRE 1.6 or newer is installed and available in path.
+#
+# Alexey Skorokhodov. 2012.   http://www.taskadapter.com
+
+if [ "$(which java)" == "" ]; then
+  echo "no java"
+  exit 1
+fi
+
+   VER=$(java -version 2>&1 | grep 'version' | sed 's/^.*"\(.*\)".*$/\1/g')
+   MAJ=$(echo $VER | awk -F. '{print $1}')
+   MIN=$(echo $VER | awk -F. '{print $2}')
+
+   FOUND_REQUIRED_JAVA=0;
+
+if [ "${MAJ}" -gt 1 ]; then
+    FOUND_REQUIRED_JAVA=1
+elif [ "${MAJ}" -eq 1 ]; then
+     ## "ge" means greater or equal
+     if [ "${MIN}" -ge 6 ]; then
+        FOUND_REQUIRED_JAVA=1
+     fi
+fi
+
+if [ "$FOUND_REQUIRED_JAVA" -eq 0 ]; then
+  echo "Task Adapter requires Java Runtime Environment 1.6 installed and available in PATH. Please install JRE 1.6 or newer."
+  echo "http://www.java.com/en/download/index.jsp"
+  exit 1
+fi
+
+echo "Starting TaskAdapter using Java ${VER}."
 java -jar launcher.jar --port=8080  --openTaskAdapterPageInWebBrowser
