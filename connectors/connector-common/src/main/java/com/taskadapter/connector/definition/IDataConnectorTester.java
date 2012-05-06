e68e23c862698a248537695427da36f97a99f984
Alexey Skorokhodov <alskor@gmail.com>
2012-05-05 18:16:24 -0700

    deleted outdated unused classes: IDataConnectorTester, JiraTester, RedmineTester

--- connectors/connector-common/src/main/java/com/taskadapter/connector/definition/IDataConnectorTester.java
+++
@@ -1,9 +0,0 @@
-package com.taskadapter.connector.definition;
-
-import com.taskadapter.model.GProject;
-
-public interface IDataConnectorTester {
-    public GProject createRandomProject(ConnectorConfig config) throws Exception;
-
-    public void deleteProject(ConnectorConfig config, String projectKey) throws Exception;
-}
