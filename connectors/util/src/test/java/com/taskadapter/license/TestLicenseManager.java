4face567aa7351237cb5042146f0ddec51809d9a
Igor Laishen <igor.lisx@gmail.com>
2012-05-09 21:33:03 +0300

    Task-32451: fixed imports

--- connectors/util/src/test/java/com/taskadapter/license/TestLicenseManager.java
+++ connectors/util/src/test/java/com/taskadapter/license/TestLicenseManager.java
@@ -6,17 +6,15 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static com.taskadapter.license.LicenseManager.Product.TASK_ADAPTER;
-import static com.taskadapter.license.LicenseManager.checkLicense;
 
 public class TestLicenseManager {
     @Test
     public void testValidTALicense() {
         try {
-            License license = checkLicense(MyIOUtils.getResourceAsString("taskadapter.license.valid"));
+            License license = LicenseManager.checkLicense(MyIOUtils.getResourceAsString("taskadapter.license.valid"));
 
             Assert.assertNotNull("license must not be null", license);
-            Assert.assertEquals("product type must be " + TASK_ADAPTER, TASK_ADAPTER, license.getProduct());
+            Assert.assertEquals("product type must be " + LicenseManager.Product.TASK_ADAPTER, LicenseManager.Product.TASK_ADAPTER, license.getProduct());
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -27,7 +25,7 @@ public class TestLicenseManager {
     @Test
     public void testInvalidTALicense() {
         try {
-            checkLicense(MyIOUtils.getResourceAsString("taskadapter.license.invalid"));
+            LicenseManager.checkLicense(MyIOUtils.getResourceAsString("taskadapter.license.invalid"));
             Assert.fail("This function call must have failed with LicenseValidationException");
 
         } catch (IOException e) {
