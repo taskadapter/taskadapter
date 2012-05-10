f1f6e23e8059ea94e9d9ab4a7631837f62f7e608
Maxim Zyukin <m.zyukin@gmail.com>
2012-05-11 00:55:05 +0700

    Deleted unused method Priorities.setValues.

--- connectors/connector-common/src/main/java/com/taskadapter/connector/Priorities.java
+++ connectors/connector-common/src/main/java/com/taskadapter/connector/Priorities.java
@@ -43,13 +43,6 @@ public class Priorities {
         }
     }
 
-    public void setValues(List<NamedKeyedObjectImpl> priorityList) {
-        prioritiesMapping = new HashMap<String, Integer>();
-        for (NamedKeyedObject priority : priorityList) {
-            prioritiesMapping.put(priority.getKey(), Integer.valueOf(priority.getName()));
-        }
-    }
-
     /**
      * @return the nearest priority value for Tracker based on the MSP integer value (priority field).
      *         Never NULL.
