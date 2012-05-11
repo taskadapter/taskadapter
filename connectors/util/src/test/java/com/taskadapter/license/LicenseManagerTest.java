8fcf8dfad30a4dc3692133f8f3866ae029e51a7c
Alexey Skorokhodov <alskor@gmail.com>
2012-05-10 18:54:54 -0700

    renamed a test class to follow standard conventions : LicenseManagerTest.java

--- connectors/util/src/test/java/com/taskadapter/license/LicenseManagerTest.java
+++ connectors/util/src/test/java/com/taskadapter/license/LicenseManagerTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import java.io.IOException;
 
 
-public class TestLicenseManager {
+public class LicenseManagerTest {
     @Test
     public void testValidSingleLicense() {
         try {
