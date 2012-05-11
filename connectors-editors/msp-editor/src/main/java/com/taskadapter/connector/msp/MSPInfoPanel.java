a56b59417872656f61a75534db3d59bff696517b
Alexey Skorokhodov <alskor@gmail.com>
2012-05-10 22:42:15 -0700

    changed design of "msp info" panel shown inside MSPEditor.

---
+++ connectors-editors/msp-editor/src/main/java/com/taskadapter/connector/msp/MSPInfoPanel.java
@@ -0,0 +1,40 @@
+package com.taskadapter.connector.msp;
+
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+
+public class MSPInfoPanel extends Panel {
+    private Label durationText;
+    private Label workText;
+
+    public MSPInfoPanel() {
+        super("MSP Text Fields to store some internal stuff");
+        buildUI();
+    }
+
+    private void buildUI() {
+        addStyleName("panelexample");
+        GridLayout layout = new GridLayout();
+        layout.setColumns(2);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+
+        layout.addComponent(new Label("Store 'Duration undefined' flag as:"));
+        durationText = new Label();
+        layout.addComponent(durationText);
+
+        layout.addComponent(new Label("Store 'Work undefined' flag as:"));
+        workText = new Label();
+        layout.addComponent(workText);
+        addComponent(layout);
+    }
+
+    public void setDurationValue(String value) {
+        durationText.setValue(value);
+    }
+
+    public void setWorkValue(String value) {
+        workText.setValue(value);
+    }
+}
