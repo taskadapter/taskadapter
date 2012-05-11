85d66edf74a44fe2a2ea8ce85e7f767084d8accd
Alexey Skorokhodov <alskor@gmail.com>
2012-05-10 22:56:41 -0700

    improved LocalModeFilePanel's design: now it takes less vertical space.

--- webshared/src/main/java/com/taskadapter/web/configeditor/file/LocalModeFilePanel.java
+++ webshared/src/main/java/com/taskadapter/web/configeditor/file/LocalModeFilePanel.java
@@ -2,13 +2,15 @@ package com.taskadapter.web.configeditor.file;
 
 import com.taskadapter.connector.msp.MSPConfig;
 import com.taskadapter.web.configeditor.EditorUtil;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 
 public class LocalModeFilePanel extends FilePanel {
-    private static final String LABEL_FILE_NAME = "Input File name:";
+    private static final String LABEL_FILE_NAME = "Input file:";
     private static final String TOOLTIP_FILE_NAME = "Microsoft Project file name to load the data from (MPP or XML file)";
 
-    private static final String LABEL_OUTPUT_FILE_NAME = "Output file name:";
+    private static final String LABEL_OUTPUT_FILE_NAME = "Output file:";
     private static final String TOOLTIP_OUTPUT_FILE_NAME = "Microsoft Project file name to save the data to (only XML format is supported)";
 
     public LocalModeFilePanel() {
@@ -20,15 +22,23 @@ public class LocalModeFilePanel extends FilePanel {
     private TextField outputFileNameField;
 
     private void buildUI() {
-        inputFileNameField = createFileName(LABEL_FILE_NAME, TOOLTIP_FILE_NAME);
-        outputFileNameField = createFileName(LABEL_OUTPUT_FILE_NAME, TOOLTIP_OUTPUT_FILE_NAME);
-        addComponent(inputFileNameField);
-        addComponent(outputFileNameField);
+        GridLayout layout = new GridLayout();
+        addComponent(layout);
+        layout.setColumns(2);
+        layout.setSpacing(true);
+
+        layout.addComponent(new Label(LABEL_FILE_NAME));
+        inputFileNameField = createFileName(TOOLTIP_FILE_NAME);
+        layout.addComponent(inputFileNameField);
+
+        layout.addComponent(new Label(LABEL_OUTPUT_FILE_NAME));
+        outputFileNameField = createFileName(TOOLTIP_OUTPUT_FILE_NAME);
+        layout.addComponent(outputFileNameField);
     }
 
 
-    private TextField createFileName(String label, String tooltip) {
-        final TextField field = new TextField(label);
+    private TextField createFileName(String tooltip) {
+        final TextField field = new TextField();
         field.setDescription(tooltip);
         field.addStyleName("msp-file-name-textfield");
         return field;
