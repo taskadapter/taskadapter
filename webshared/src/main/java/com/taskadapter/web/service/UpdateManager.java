a965836b935bab325d797c99a1361c74a3462acd
Alexey Skorokhodov <alskor@gmail.com>
2012-05-22 17:52:11 -0700

    set version to 2.0.0-RC2

--- webshared/src/main/java/com/taskadapter/web/service/UpdateManager.java
+++ webshared/src/main/java/com/taskadapter/web/service/UpdateManager.java
@@ -7,7 +7,7 @@ public class UpdateManager {
 
     public UpdateManager() {
         // TODO hardcoded current version
-        currentVersion = "2.0.0-RC1";
+        currentVersion = "2.0.0-RC2";
 
 // disabled for now to not generate a lot of useless "check version" requests to the server.
 // we'll enable before the release.
