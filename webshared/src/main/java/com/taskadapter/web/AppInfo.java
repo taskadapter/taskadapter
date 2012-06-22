5ab3b57f14ed478983b4d97cb660654766d43920
Alexey Skorokhodov <alskor@gmail.com>
2012-06-21 23:18:05 -0700

    #71375 app version should be stored in 1 place.
    Added AppInfo class. Added version.properties template, which is populated by Maven during resources processing stage.

---
+++ webshared/src/main/java/com/taskadapter/web/AppInfo.java
@@ -0,0 +1,22 @@
+package com.taskadapter.web;
+
+public class AppInfo {
+    private String version;
+    private String buildDate;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(final String version) {
+        this.version = version;
+    }
+
+    public String getBuildDate() {
+        return buildDate;
+    }
+
+    public void setBuildDate(final String buildDate) {
+        this.buildDate = buildDate;
+    }
+}
