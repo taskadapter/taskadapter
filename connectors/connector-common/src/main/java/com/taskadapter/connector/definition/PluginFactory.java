93938d0df3f8d2c99494a5369554a31d55db1ef6
Alexey Skorokhodov <alskor@gmail.com>
2012-09-26 18:40:08 +0700

    deleted useless "public" method declarations. deleted an outdated TODO element.

--- connectors/connector-common/src/main/java/com/taskadapter/connector/definition/PluginFactory.java
+++ connectors/connector-common/src/main/java/com/taskadapter/connector/definition/PluginFactory.java
@@ -9,28 +9,30 @@ import com.google.gson.JsonParseException;
  * plugin factory and leave this as a "connector factory" item.
  */
 public interface PluginFactory<C extends ConnectorConfig> {
-    public Connector<? extends C> createConnector(C config);
+    Connector<? extends C> createConnector(C config);
 
-    //TODO delete method with the same name from Connector class.
-    public Descriptor getDescriptor();
+    Descriptor getDescriptor();
 
     /**
      * Serializes a config to a Json Element.
+     *
      * @param config config to serialize.
      * @return serialized config.
      */
-    public JsonElement writeConfig(C config);
-    
+    JsonElement writeConfig(C config);
+
     /**
      * Reads a config.
+     *
      * @param config config to read.
      * @return config from input.
      * @throws JsonParseException if config is in invalid format.
      */
-    public C readConfig(JsonElement config) throws JsonParseException;
-    
+    C readConfig(JsonElement config) throws JsonParseException;
+
     /**
      * Creates a default ("almost empty") connector config.
+     *
      * @return new connector config.
      */
     C createDefaultConfig();
