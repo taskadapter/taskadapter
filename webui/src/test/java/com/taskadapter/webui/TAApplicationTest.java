de3b54a7aebc35bb04bb078ea896369f4b35a58e
Alexey Skorokhodov <alskor@gmail.com>
2012-10-06 17:51:54 -0700

    added Mockito dependency to webui/pom.xml

--- webui/src/test/java/com/taskadapter/webui/TAApplicationTest.java
+++ webui/src/test/java/com/taskadapter/webui/TAApplicationTest.java
@@ -6,13 +6,15 @@ import org.mockito.Mockito;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.mockito.Mockito.mock;
+
 public class TAApplicationTest {
 
     @Test
     public void applicationRespondsToRequestsWithoutInit() {
         TAApplication application = new TAApplication();
-        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
         application.onRequestStart(request, response);
     }
 }
