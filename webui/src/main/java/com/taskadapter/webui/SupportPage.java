5b7dc1352084477e86e8afcece6eec824284ad46
Alexey Skorokhodov <alskor@gmail.com>
2012-10-15 21:52:14 -0700

    deleted old commented out code, which was supposed to be used to send support requests from Support page inside TA application itself. there's not a lot of value in that code.

--- webui/src/main/java/com/taskadapter/webui/SupportPage.java
+++ webui/src/main/java/com/taskadapter/webui/SupportPage.java
@@ -23,44 +23,6 @@ public class SupportPage extends Page {
         addCheckForUpdateLink();
         addBuyLicenseLink();
         addEmailLink();
-
-/*        Form feedbackForm = new Form();
-        feedbackForm.setDescription("You can send your comments to Task Adapter developers using this form");
-        feedbackForm.setImmediate(true);
-        feedbackForm.setFooter(new VerticalLayout());
-        feedbackForm.getFooter().addComponent(new Label("All fields are required"));
-
-        EmailValidator emailSenderValidator = new EmailValidator("Invalid e-mail address entered.");
-
-        HorizontalLayout buttonsBar = new HorizontalLayout();
-        buttonsBar.setHeight("26px");
-        feedbackForm.getFooter().addComponent(buttonsBar);
-
-        TextField nameSender = new TextField(FIELD_NAME);
-        nameSender.setColumns(30);
-        nameSender.setRequired(true);
-        nameSender.setRequiredError("Name is missing");
-        TextField emailSender = new TextField(FIELD_EMAIL);
-        emailSender.setColumns(30);
-        emailSender.setRequired(true);
-        emailSender.setRequiredError("E-mail address is missing");
-        emailSender.addValidator(emailSenderValidator);
-        TextArea emailMessage = new TextArea(FIELD_MESSAGE);
-        emailMessage.setColumns(30);
-        emailMessage.setRows(10);
-        emailMessage.setRequired(true);
-        emailMessage.setRequiredError("Message is missing");
-
-        feedbackForm.addField(FIELD_NAME, nameSender);
-        feedbackForm.addField(FIELD_EMAIL, emailSender);
-        feedbackForm.addField(FIELD_MESSAGE, emailMessage);
-
-        Button sendButton = new Button("Send", feedbackForm, "commit");
-        buttonsBar.addComponent(sendButton);
-        buttonsBar.setComponentAlignment(sendButton, Alignment.TOP_RIGHT);
-        buttonsBar.addComponent(new Button("Cancel"));
-
-        layout.addComponent(feedbackForm);*/
     }
 
     private void addCurrentVersion() {
