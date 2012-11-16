69cb074f8c827cde850463d11d2a8c52c0832d6e
maxkar <Maxim.Karvonen@gmail.com>
2012-11-16 15:30:13 +0400

    * Better display for invalid configs

--- webui/src/main/java/com/taskadapter/webui/UniConfigExport.java
+++ webui/src/main/java/com/taskadapter/webui/UniConfigExport.java
@@ -1,5 +1,6 @@
 package com.taskadapter.webui;
 
+import com.taskadapter.connector.definition.exceptions.BadConfigException;
 import com.taskadapter.web.data.Messages;
 import com.taskadapter.web.service.Services;
 import com.taskadapter.web.uiapi.UIConnectorConfig;
@@ -16,15 +17,15 @@ import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 
 /**
- * Controller for a single export. 
- *
+ * Controller for a single export.
+ * 
  */
 final class UniConfigExport {
     private final Messages messages;
     private final Services services;
     private final Navigator navigator;
     private final UISyncConfig syncConfig;
-    
+
     private final HorizontalLayout ui;
 
     public UniConfigExport(Messages messages, Services services,
@@ -33,19 +34,23 @@ final class UniConfigExport {
         this.services = services;
         this.navigator = navigator;
         this.syncConfig = uiSyncConfig;
-        
+
         ui = createUI();
     }
-    
+
     private HorizontalLayout createUI() {
         final HorizontalLayout res = new HorizontalLayout();
         res.setWidth(274, Sizeable.UNITS_PIXELS);
+
+        final boolean isValid = checkValid();
+
         res.addStyleName("uniExportPanel");
-        
+        res.addStyleName(isValid ? "valid" : "invalid");
+
         final UIConnectorConfig config1;
         final UIConnectorConfig config2;
         final String assetName;
-        
+
         if (syncConfig.isReversed()) {
             config1 = syncConfig.getConnector2();
             config2 = syncConfig.getConnector1();
@@ -55,36 +60,47 @@ final class UniConfigExport {
             config2 = syncConfig.getConnector2();
             assetName = "img/arrow_right.png";
         }
-        
+
         final Label leftLabel = createLabel(config1);
         final Label rightLabel = createLabel(config2);
         final Embedded actionLabel = new Embedded(null, new ThemeResource(
                 assetName));
-        
+
         leftLabel.addStyleName("left-label");
         rightLabel.addStyleName("right-label");
-        
+
         res.addComponent(leftLabel);
         res.addComponent(actionLabel);
         res.addComponent(rightLabel);
 
-        
         res.setExpandRatio(leftLabel, 1.0f);
         res.setExpandRatio(rightLabel, 1.0f);
         res.setSpacing(true);
-        
+
         res.setComponentAlignment(leftLabel, Alignment.MIDDLE_RIGHT);
         res.setComponentAlignment(actionLabel, Alignment.MIDDLE_CENTER);
         res.setComponentAlignment(rightLabel, Alignment.MIDDLE_LEFT);
-        
-        res.addListener(new LayoutEvents.LayoutClickListener() {
-            @Override
-            public void layoutClick(LayoutClickEvent event) {
-                export();
-            }
-        });
-        
+
+        if (isValid) {
+            res.addListener(new LayoutEvents.LayoutClickListener() {
+                @Override
+                public void layoutClick(LayoutClickEvent event) {
+                    export();
+                }
+            });
+        }
+
         return res;
+    }
+
+    private boolean checkValid() {
+        try {
+            syncConfig.getConnector1().validateForLoad();
+            syncConfig.getConnector2().validateForSave();
+        } catch (BadConfigException e) {
+            return false;
+        }
+        return true;
     }
 
     private Label createLabel(UIConnectorConfig connector) {
@@ -92,7 +108,7 @@ final class UniConfigExport {
         res.setWidth(100, Sizeable.UNITS_PERCENTAGE);
         return res;
     }
-    
+
     void export() {
         new Exporter(messages, services, navigator, syncConfig).export();
     }
