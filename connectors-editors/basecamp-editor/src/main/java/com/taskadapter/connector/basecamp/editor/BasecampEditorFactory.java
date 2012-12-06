6bc8e5b522598049c4042c7ebdee252b7ff168e0
Alexey Skorokhodov <alskor@gmail.com>
2012-12-06 00:00:31 -0800

    added "show project info" button to Basecamp editor

--- connectors-editors/basecamp-editor/src/main/java/com/taskadapter/connector/basecamp/editor/BasecampEditorFactory.java
+++ connectors-editors/basecamp-editor/src/main/java/com/taskadapter/connector/basecamp/editor/BasecampEditorFactory.java
@@ -1,14 +1,20 @@
 package com.taskadapter.connector.basecamp.editor;
 
 import com.taskadapter.connector.basecamp.BasecampConfig;
-import com.taskadapter.connector.definition.WebServerInfo;
 import com.taskadapter.connector.definition.exceptions.BadConfigException;
 import com.taskadapter.web.PluginEditorFactory;
 import com.taskadapter.web.WindowProvider;
+import com.taskadapter.web.configeditor.EditorUtil;
 import com.taskadapter.web.configeditor.server.ServerPanelWithAPIKey;
 import com.taskadapter.web.service.Services;
 import com.vaadin.data.util.MethodProperty;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 
 import java.util.List;
 
@@ -16,6 +22,20 @@ public class BasecampEditorFactory implements PluginEditorFactory<BasecampConfig
     @Override
     public ComponentContainer getMiniPanelContents(WindowProvider windowProvider, Services services, BasecampConfig config, List<BasecampConfig> relatedConfigs) {
 
+        Panel panel = createServerPanel(config);
+        Panel projectPanel = createProjectPanel(windowProvider, config);
+
+        GridLayout grid = new GridLayout();
+        grid.setColumns(2);
+        grid.setMargin(true);
+        grid.setSpacing(true);
+
+        grid.addComponent(panel);
+        grid.addComponent(projectPanel);
+        return grid;
+    }
+
+    private Panel createServerPanel(BasecampConfig config) {
         Panel panel = new Panel("Server Info");
         ServerPanelWithAPIKey redmineServerPanel = new ServerPanelWithAPIKey(new MethodProperty<String>(config, "label"),
                 new MethodProperty<String>(config, "serverUrl"),
@@ -24,45 +44,51 @@ public class BasecampEditorFactory implements PluginEditorFactory<BasecampConfig
                 new MethodProperty<String>(config.getAuth(), "apiKey"),
                 new MethodProperty<Boolean>(config.getAuth(), "useAPIKeyInsteadOfLoginPassword"));
         panel.addComponent(redmineServerPanel);
+        return panel;
+    }
+
+    private Panel createProjectPanel(final WindowProvider windowProvider, final BasecampConfig config) {
+        Panel projectPanel = new Panel("Project");
 
         GridLayout grid = new GridLayout();
-        grid.setColumns(2);
+        grid.setColumns(3);
         grid.setMargin(true);
         grid.setSpacing(true);
 
-        grid.addComponent(panel);
-
-        Panel projectPanel = new Panel("Project");
-
-        GridLayout projectGrid = new GridLayout();
-        projectGrid.setColumns(2);
-        projectGrid.setMargin(true);
-        projectGrid.setSpacing(true);
-
-        projectPanel.addComponent(projectGrid);
-
-        grid.addComponent(projectPanel);
+        projectPanel.addComponent(grid);
 
         Label accountIdLabel = new Label("Account Id:");
-        projectGrid.addComponent(accountIdLabel);
+        grid.addComponent(accountIdLabel);
         TextField accountIdField = new TextField();
         accountIdField.setPropertyDataSource(new MethodProperty<String>(config, "accountId"));
-        projectGrid.addComponent(accountIdField);
+        grid.addComponent(accountIdField);
+        grid.addComponent(new Label(""));
 
         Label projectKeyLabel = new Label("Project key:");
-        projectGrid.addComponent(projectKeyLabel);
+        grid.addComponent(projectKeyLabel);
         TextField projectKeyField = new TextField();
         projectKeyField.setPropertyDataSource(new MethodProperty<String>(config, "projectKey"));
-        projectGrid.addComponent(projectKeyField);
+        grid.addComponent(projectKeyField);
+        Button infoButton = EditorUtil.createButton("Info", "View the project info",
+                new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(Button.ClickEvent event) {
+                        new ShowProjectElement(windowProvider, config, BasecampEditorFactory.this).loadProject();
+                    }
+                }
+        );
+        grid.addComponent(infoButton);
+        grid.setComponentAlignment(infoButton, Alignment.MIDDLE_CENTER);
 
         Label todoListKey = new Label("Todo list key:");
-        projectGrid.addComponent(todoListKey);
+        grid.addComponent(todoListKey);
         TextField todoListField = new TextField();
         todoListField.setPropertyDataSource(new MethodProperty<String>(config, "todoKey"));
-        projectGrid.addComponent(todoListField);
-
-        return grid;
+        grid.addComponent(todoListField);
+        grid.addComponent(new Label(""));
+        return projectPanel;
     }
+
 
     @Override
     public void validateForSave(BasecampConfig config) throws BadConfigException {
---
+++ connectors-editors/basecamp-editor/src/main/java/com/taskadapter/connector/basecamp/editor/ShowProjectElement.java
@@ -0,0 +1,57 @@
+package com.taskadapter.connector.basecamp.editor;
+
+import com.taskadapter.connector.basecamp.BasecampConfig;
+import com.taskadapter.connector.basecamp.BasecampUtils;
+import com.taskadapter.connector.basecamp.transport.BaseCommunicator;
+import com.taskadapter.connector.basecamp.transport.ObjectAPIFactory;
+import com.taskadapter.connector.definition.exceptions.BadConfigException;
+import com.taskadapter.connector.definition.exceptions.ConnectorException;
+import com.taskadapter.model.GProject;
+import com.taskadapter.web.ExceptionFormatter;
+import com.taskadapter.web.WindowProvider;
+import com.taskadapter.web.configeditor.EditorUtil;
+
+public class ShowProjectElement {
+    private final WindowProvider windowProvider;
+    private final BasecampConfig config;
+    private final ExceptionFormatter exceptionFormatter;
+
+    public ShowProjectElement(WindowProvider windowProvider, BasecampConfig config, ExceptionFormatter exceptionFormatter) {
+        this.windowProvider = windowProvider;
+        this.config = config;
+        this.exceptionFormatter = exceptionFormatter;
+    }
+
+    void loadProject() {
+        final ObjectAPIFactory factory = new ObjectAPIFactory(
+                new BaseCommunicator());
+        try {
+            GProject project = BasecampUtils.loadProject(factory, config);
+            showProjectInfo(windowProvider, project);
+
+        } catch (BadConfigException e) {
+            String localizedMessage = exceptionFormatter.formatError(e);
+            windowProvider.getWindow().showNotification(localizedMessage);
+        } catch (ConnectorException e) {
+            String localizedMessage = exceptionFormatter.formatError(e);
+            windowProvider.getWindow().showNotification("Oops", localizedMessage);
+        }
+    }
+
+    private void showProjectInfo(WindowProvider windowProvider, GProject project) {
+        String msg = "<BR>Key:  " + project.getKey() +
+                "<BR>Name: " + project.getName();
+        msg += addNullSafe("Description", project.getDescription());
+
+        EditorUtil.show(windowProvider.getWindow(), "Project Info", msg);
+    }
+
+    private String addNullSafe(String label, String fieldValue) {
+        String msg = "<BR>" + label + ": ";
+        if (fieldValue != null) {
+            msg += fieldValue;
+        }
+        return msg;
+    }
+
+}
