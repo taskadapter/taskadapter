a0a7ff38c31dac07bb43cc3b51e24eea38a6eb55
Alexey Skorokhodov <alskor@gmail.com>
2013-05-10 20:11:16 -0700

    updated Basecamp test settings (new temporary account)

--- connectors/connector-basecamp/src/test/java/com/taskadapter/connector/basecamp/TestBasecampConfig.java
+++ connectors/connector-basecamp/src/test/java/com/taskadapter/connector/basecamp/TestBasecampConfig.java
@@ -1,10 +1,10 @@
 package com.taskadapter.connector.basecamp;
 
 class TestBasecampConfig {
-    private static final String USER_ID = "2192889";
-    private static final String USER_LOGIN = "yosxkzqx@sharklasers.com";
+    private static final String USER_ID = "2260133";
+    private static final String USER_LOGIN = "rtfxmqks@sharklasers.com";
     private static final String USER_PASSWORD = "123123123";
-    private static final String PROJECT_KEY = "2337516";
+    private static final String PROJECT_KEY = "2834112";
 
     static BasecampConfig create() {
         BasecampConfig config = new BasecampConfig();
