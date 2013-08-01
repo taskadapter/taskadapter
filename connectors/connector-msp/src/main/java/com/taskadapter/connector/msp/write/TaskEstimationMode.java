e359d55f5d17436e67a1eeb0492df0bb2332bc12
maxkar <Maxim.Karvonen@gmail.com>
2013-08-01 12:08:19 +0400

    * Added javadoc for WILD_GUESS

--- connectors/connector-msp/src/main/java/com/taskadapter/connector/msp/write/TaskEstimationMode.java
+++ connectors/connector-msp/src/main/java/com/taskadapter/connector/msp/write/TaskEstimationMode.java
@@ -4,5 +4,24 @@ package com.taskadapter.connector.msp.write;
  * Task estimation mode. Describes how to estimate a task time.
  */
 public enum TaskEstimationMode {
-    TASK_TIME, WILD_GUESS, NO_ESTIMATE
+    /**
+     * Use GTask.estimatedTime as time estimation.
+     */
+    TASK_TIME, 
+    /**
+     * Guess estimation based on a task structure but not on explicitly set
+     * estimated time.
+     * Task estimator may use any of the following (but not limited to) 
+     * approaches:
+     * <ul>
+     *   <li>Use fixed some default value
+     *   <li>Use value configured by a user
+     *   <li>Estimate time using estimated time for subtasks.
+     * </ul>
+     */
+    WILD_GUESS,     
+    /**
+     * Task requires no estimated time.
+     */
+    NO_ESTIMATE
 }
