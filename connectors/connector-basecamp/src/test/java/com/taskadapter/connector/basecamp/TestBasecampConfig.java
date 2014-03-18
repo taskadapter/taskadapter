31f28553cb5780d7b9685670940e498266733028
Alexey Skorokhodov <alskor@gmail.com>
2014-03-17 23:05:18 -0700

    update Basecamp test login because the old one expired again.

--- connectors/connector-basecamp/src/test/java/com/taskadapter/connector/basecamp/TestBasecampConfig.java
+++ connectors/connector-basecamp/src/test/java/com/taskadapter/connector/basecamp/TestBasecampConfig.java
@@ -3,10 +3,10 @@ package com.taskadapter.connector.basecamp;
 class TestBasecampConfig {
     public static final String USER_FIRST_NAME = "Tester";
 
-    private static final String USER_ID = "2515207";
-    private static final String USER_LOGIN = "dgkcc9q+luqv9c@sharklasers.com";
+    private static final String USER_ID = "2584780";
+    private static final String USER_LOGIN = "cwg0l20+2q3h7g@sharklasers.com";
     private static final String USER_PASSWORD = "123123123";
-    private static final String PROJECT_KEY = "4768037";
+    private static final String PROJECT_KEY = "5375264";
 
     static BasecampConfig create() {
         BasecampConfig config = new BasecampConfig();
