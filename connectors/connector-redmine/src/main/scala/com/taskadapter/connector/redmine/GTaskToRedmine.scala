e59ea9aed89108bfad669e09377c2aea00a5dcd4
Alexey Skorokhodov <alskor@gmail.com>
2017-07-08 14:17:23 -0700

    converted GTaskToRedmine Java->Scala
    also replaced "Generic" field names in it with Redmine-specific ones.

---
+++ connectors/connector-redmine/src/main/scala/com/taskadapter/connector/redmine/GTaskToRedmine.scala
@@ -0,0 +1,131 @@
+package com.taskadapter.connector.redmine
+
+import java.util
+import java.util.Date
+
+import com.google.common.base.Strings
+import com.taskadapter.connector.common.data.ConnectorConverter
+import com.taskadapter.model.GTask
+import com.taskadapter.redmineapi.bean._
+
+import scala.collection.JavaConverters._
+
+class GTaskToRedmine(config: RedmineConfig, priorities: util.Map[String, Integer], project: Project,
+                     users: util.List[User], customFieldDefinitions: util.List[CustomFieldDefinition],
+                     statusList: util.List[IssueStatus], versions: util.List[Version])
+  extends ConnectorConverter[GTask, Issue] {
+
+  private def parseIntOrNull(s: String): Integer = {
+    try
+      s.toInt
+    catch {
+      case e@(_: NumberFormatException | _: NullPointerException) =>
+        null
+    }
+  }
+
+  private def convertToRedmineIssue(task: GTask): Issue = {
+    val key = task.getKey
+    val numericKey = parseIntOrNull(key)
+    val issue = IssueFactory.create(numericKey)
+    issue.setProject(project)
+
+    task.getFields.asScala.foreach { x =>
+      processField(issue, x._1, x._2)
+    }
+    issue
+  }
+
+  private def processField(issue: Issue, fieldName: String, value: Any): Unit = {
+    fieldName match {
+      case "PARENT_KEY" => issue.setParentId(parseIntOrNull(value.asInstanceOf[String]))
+      case "ID" => // TODO TA3 review this. ignore ID field because it MAYBE does not need to be provided when saving?
+      case "RELATIONS" => // processed in another place (for now?)
+      case "CHILDREN" => // processed in another place (for now?)
+
+      case RedmineField.summary => issue.setSubject(value.asInstanceOf[String])
+      case RedmineField.startDate => issue.setStartDate(value.asInstanceOf[Date])
+      case RedmineField.dueDate => issue.setDueDate(value.asInstanceOf[Date])
+      case RedmineField.estimatedTime => issue.setEstimatedHours(value.asInstanceOf[Float])
+
+      case RedmineField.doneRatio => issue.setDoneRatio(value.asInstanceOf[Integer])
+      case RedmineField.taskType =>
+        var trackerName = value.asInstanceOf[String]
+        if (Strings.isNullOrEmpty(trackerName)) trackerName = config.getDefaultTaskType
+        issue.setTracker(project.getTrackerByName(trackerName))
+      case RedmineField.taskStatus =>
+        processTaskStatus(issue, value.asInstanceOf[String])
+      case RedmineField.description =>
+        issue.setDescription(value.asInstanceOf[String])
+      case RedmineField.priority =>
+        val priority = value.asInstanceOf[Integer]
+        if (priority != null) {
+          val priorityName = config.getPriorities.getPriorityByMSP(priority)
+          val `val` = priorities.get(priorityName)
+          if (`val` != null) {
+            issue.setPriorityId(`val`)
+            issue.setPriorityText(priorityName)
+          }
+        }
+      case RedmineField.targetVersion =>
+        val version = getVersionByName(value.asInstanceOf[String])
+        issue.setTargetVersion(version)
+      case RedmineField.createdOn => issue.setCreatedOn(value.asInstanceOf[Date])
+      case RedmineField.updatedOn => issue.setUpdatedOn(value.asInstanceOf[Date])
+      case RedmineField.assignee => processAssignee(issue, value)
+      case _ =>
+        // all known fields are processed. considering this a custom field
+        val customFieldId = CustomFieldDefinitionFinder.findCustomFieldId(customFieldDefinitions, fieldName)
+        if (customFieldId == null) throw new RuntimeException("Cannot find Id for custom field " + fieldName + ". Known fields are:" + customFieldDefinitions)
+        val customField = CustomFieldFactory.create(customFieldId, fieldName, value.asInstanceOf[String])
+        issue.addCustomField(customField)
+    }
+  }
+
+  private def getVersionByName(versionName: String): Version = {
+    if (versions == null || versionName == null) return null
+    versions.asScala.find(_.getName == versionName).orNull
+  }
+
+  private def processAssignee(redmineIssue: Issue, value: Any): Unit = {
+    val userLoginName = value.asInstanceOf[String]
+    if (!Strings.isNullOrEmpty(userLoginName)) {
+      val rmAss = findRedmineUserInCache(userLoginName)
+      redmineIssue.setAssignee(rmAss)
+    }
+  }
+
+  private def processTaskStatus(issue: Issue, value: String): Unit = {
+    var statusName = value
+    if (statusName == null) statusName = config.getDefaultTaskStatus
+    val status = getStatusByName(statusName)
+    if (status != null) {
+      issue.setStatusId(status.getId)
+      issue.setStatusName(status.getName)
+    }
+  }
+
+  /**
+    * @return NULL if the user is not found or if "users" weren't previously set via setUsers() method
+    */
+  private def findRedmineUserInCache(login: String): User = {
+    if (users == null || Strings.isNullOrEmpty(login)) return null
+    users.asScala
+      .find(u => login.equalsIgnoreCase(u.getLogin) || login.equalsIgnoreCase(u.getFullName))
+      .orNull
+  }
+
+  /**
+    * @return NULL if the status is not found or if "statusList" weren't previously set via setStatusList() method
+    */
+  private def getStatusByName(name: String): IssueStatus = {
+    if (statusList == null || name == null) return null
+    statusList.asScala.find(s => s.getName.equalsIgnoreCase(name)).orNull
+  }
+
+  override def convert(source: GTask): Issue = {
+    convertToRedmineIssue(source)
+  }
+}
+
+
