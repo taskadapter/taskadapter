66793e7add8b54a8a5991faca5962283c9e53e06
Alexey Skorokhodov <alskor@gmail.com>
2017-07-09 20:47:32 -0700

    converted GTaskToJira to Scala for better type safety in switch

---
+++ connectors/connector-jira/src/main/scala/com/taskadapter/connector/jira/GTaskToJira.scala
@@ -0,0 +1,108 @@
+package com.taskadapter.connector.jira
+
+import java.util
+
+import com.atlassian.jira.rest.client.api.domain.input.{ComplexIssueInputFieldValue, FieldInput, IssueInputBuilder}
+import com.atlassian.jira.rest.client.api.domain.{BasicComponent, BasicPriority, IssueFieldId, IssueType, Priority, TimeTracking, Version}
+import com.google.common.collect.ImmutableList
+import com.taskadapter.connector.common.data.ConnectorConverter
+import com.taskadapter.connector.definition.exceptions.ConnectorException
+import com.taskadapter.model.GTask
+import org.joda.time.DateTime
+import scala.collection.JavaConverters._
+
+object GTaskToJira {
+  def getVersion(versions: Iterable[Version], versionName: String): Version = {
+    versions.find(_.getName == versionName).orNull
+  }
+
+  def getComponent(objects: Iterable[BasicComponent], name: String): BasicComponent = {
+    objects.find(_.getName == name).orNull
+  }
+}
+
+class GTaskToJira(config: JiraConfig, issueTypeList: java.lang.Iterable[IssueType],
+                  versions: java.lang.Iterable[Version],
+                  components: java.lang.Iterable[BasicComponent],
+                  jiraPriorities: java.lang.Iterable[Priority])
+  extends ConnectorConverter[GTask, IssueWrapper] {
+
+  jiraPriorities.forEach { p =>
+    priorities.put(p.getName, p)
+  }
+  final private val priorities = new util.HashMap[String, BasicPriority]
+
+  private def convertToJiraIssue(task: GTask) = {
+    val issueInputBuilder = new IssueInputBuilder(config.getProjectKey, findIssueTypeId(task))
+    if (task.getParentKey != null) {
+      // See http://stackoverflow.com/questions/14699893/how-to-create-subtasks-using-jira-rest-java-client
+      val parent = new util.HashMap[String, AnyRef]
+      parent.put("key", task.getParentKey)
+      val parentField = new FieldInput("parent", new ComplexIssueInputFieldValue(parent))
+      issueInputBuilder.setFieldInput(parentField)
+    }
+    import scala.collection.JavaConversions._
+    for (row <- task.getFields.entrySet) {
+      processField(issueInputBuilder, row.getKey, row.getValue)
+    }
+    val affectedVersion = GTaskToJira.getVersion(versions, config.getAffectedVersion)
+    val fixForVersion = GTaskToJira.getVersion(versions, config.getFixForVersion)
+    val component = GTaskToJira.getComponent(components, config.getComponent)
+    if (affectedVersion != null) issueInputBuilder.setAffectedVersions(ImmutableList.of(affectedVersion))
+    if (fixForVersion != null) issueInputBuilder.setFixVersions(ImmutableList.of(fixForVersion))
+    if (component != null) issueInputBuilder.setComponents(ImmutableList.of(component))
+    val issueInput = issueInputBuilder.build
+    new IssueWrapper(task.getKey, issueInput)
+  }
+
+  private def processField(issueInputBuilder: IssueInputBuilder, fieldName: String, value: Any) : Unit = {
+    fieldName match {
+      case JiraField.summary.name => issueInputBuilder.setSummary(value.asInstanceOf[String])
+      case JiraField.description.name => issueInputBuilder.setDescription(value.asInstanceOf[String])
+      case JiraField.dueDate.name => if (value != null) {
+        val dueDateTime = new DateTime(value)
+        issueInputBuilder.setDueDate(dueDateTime)
+      }
+      case JiraField.assignee.name => if (value != null) issueInputBuilder.setAssigneeName(value.asInstanceOf[String])
+      case JiraField.priority.name =>
+        val priorityNumber = value.asInstanceOf[Integer]
+        val jiraPriorityName = config.getPriorities.getPriorityByMSP(priorityNumber)
+        if (!jiraPriorityName.isEmpty) {
+          val priority = priorities.get(jiraPriorityName)
+          if (priority != null) issueInputBuilder.setPriority(priority)
+        }
+      case JiraField.estimatedTime.name => if (value != null) {
+        val estimatedHours = value.asInstanceOf[Float]
+        val timeTracking = new TimeTracking(Math.round(estimatedHours * 60), null, null)
+        issueInputBuilder.setFieldValue(IssueFieldId.TIMETRACKING_FIELD.id, timeTracking)
+      }
+      case JiraField.environment.name => issueInputBuilder.setFieldValue(fieldName, value)
+      case _ =>
+    }
+  }
+
+  /**
+    * Finds an issue type id to use.
+    *
+    * @param task task to get an issue id.
+    * @return issue type id.
+    */
+  private def findIssueTypeId(task: GTask): Long = {
+    // Use explicit task type when possible.
+    val value = task.getValue(JiraField.taskType.name)
+    val explicitTypeId = getIssueTypeIdByName(value.asInstanceOf[String])
+    if (explicitTypeId != null) return explicitTypeId
+    // Use default type for the task when
+    getIssueTypeIdByName(
+      if (task.getParentKey == null) config.getDefaultTaskType
+      else config.getDefaultIssueTypeForSubtasks
+    )
+  }
+
+  private def getIssueTypeIdByName(issueTypeName: String) = {
+    issueTypeList.asScala.find(i => i.getName == issueTypeName).map(_.getId).orNull
+  }
+
+  @throws[ConnectorException]
+  override def convert(source: GTask): IssueWrapper = convertToJiraIssue(source)
+}
--- connectors/connector-jira/src/main/scala/com/taskadapter/connector/jira/JiraField.scala
+++ connectors/connector-jira/src/main/scala/com/taskadapter/connector/jira/JiraField.scala
@@ -9,20 +9,20 @@ import scala.collection.JavaConverters._
   */
 object JiraField {
 
-  def summary = Field("Summary")
-  def description = Field("Description")
-  def taskType = Field("Task Type")
+  val summary = Field("Summary")
+  val description = Field("Description")
+  val taskType = Field("Task Type")
   /* newer JIRA versions (like 6.4.11) does not have "timetracking" field
      enabled for tasks by default. let's unselect this field by default
      to avoid user confusion.
    */
-  def estimatedTime = Field.float("Estimated Time") // should not be selected by default
-  def assignee = Field("Assignee")
-  def dueDate = Field.date("Due Date")
-  def priority = Field("Priority")
-  def environment = Field("Environment") // should not be selected by default
+  val estimatedTime = Field.float("Estimated Time") // should not be selected by default
+  val assignee = Field("Assignee")
+  val dueDate = Field.date("Due Date")
+  val priority = Field("Priority")
+  val environment = Field("Environment") // should not be selected by default
 
-  def fields = List(summary, description, taskType, estimatedTime, assignee, dueDate, priority, environment)
+  val fields = List(summary, description, taskType, estimatedTime, assignee, dueDate, priority, environment)
 
   def fieldsAsJava(): util.List[Field] = fields.asJava
 }
