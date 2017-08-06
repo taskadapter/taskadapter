03f12d7dfe4a3fe77532b47cfae142d18c0e5fb6
Alexey Skorokhodov <alskor@gmail.com>
2017-08-06 08:58:54 -0700

    added a test to verify config file is deleted

--- webui/src/test/scala/com/taskadapter/webui/config/ConfigStorageTest.scala
+++ webui/src/test/scala/com/taskadapter/webui/config/ConfigStorageTest.scala
@@ -3,23 +3,33 @@ package com.taskadapter.webui.config
 import com.taskadapter.config.ConfigStorage
 import com.taskadapter.web.uiapi.ConfigId
 import com.taskadapter.webui.uiapi.TempFolder
-import org.junit.Assert.assertNotNull
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
 
 @RunWith(classOf[JUnitRunner])
 class ConfigStorageTest extends FunSpec with Matchers with TempFolder {
-  private val ENCRYPTED = "test_encrypted"
+  private val configName = "some_config_name"
   private val login = "autotest"
 
   it("config can be created in storage") {
     withTempFolder { folder =>
       val storage = new ConfigStorage(folder)
-      storage.createNewConfig(login, ENCRYPTED, "jira", "value1", "jira", "value2", "mappings?")
-      val configId = ConfigId(login, ENCRYPTED)
-      val testConfigFile = storage.getConfig(configId)
-      assertNotNull("Test config file not found (might not be saved)", testConfigFile)
+      val path = storage.createNewConfig(login, configName, "jira", "value1", "jira", "value2", "mappings?")
+      val configId = ConfigId(login, path)
+      val config = storage.getConfig(configId)
+      config.isDefined shouldBe true
+      config.get.getId shouldBe path
+    }
+  }
+
+  it("config is deleted") {
+    withTempFolder { folder =>
+      val storage = new ConfigStorage(folder)
+      storage.createNewConfig(login, configName, "jira", "value1", "jira", "value2", "mappings?")
+      val configId = ConfigId(login, configName)
+      storage.delete(configId)
+      storage.getConfig(configId) shouldBe None
     }
   }
 }
