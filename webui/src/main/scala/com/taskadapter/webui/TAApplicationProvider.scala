06598c17e38b09a33dc238b9c68660b455867762
Alexey Skorokhodov <alskor@gmail.com>
2017-08-11 20:37:51 -0700

    skip GoogleAnalytics reporting when started from IDEA (/exploded/ war)

---
+++ webui/src/main/scala/com/taskadapter/webui/TAApplicationProvider.scala
@@ -0,0 +1,94 @@
+package com.taskadapter.webui
+
+import java.io.File
+
+import com.taskadapter.auth.BasicCredentialsManager
+import com.taskadapter.auth.cred.FSCredentialStore
+import com.taskadapter.webui.service.{EditorManager, Preservices}
+import com.vaadin.server.{UIClassSelectionEvent, UICreateEvent, UIProvider}
+import com.vaadin.ui.UI
+import org.slf4j.LoggerFactory
+import org.vaadin.googleanalytics.tracking.GoogleAnalyticsTracker
+
+/**
+  * Provider of the Task Adapter application.
+  */
+object TAApplicationProvider {
+  def withGoogleAnalytics: TAApplicationProvider = {
+    new TAApplicationProvider(true)
+  }
+
+  def skipGoogleAnalytics: TAApplicationProvider = {
+    new TAApplicationProvider(false)
+  }
+}
+
+/**
+  * Creates a new task application provider. Initializes all core services
+  * (like credentials manager, etc...).
+  */
+class TAApplicationProvider(reportGoogleAnalytics: Boolean) extends UIProvider {
+  val GOOGLE_ANALYTICS_ID = "UA-3768502-12"
+  val log = LoggerFactory.getLogger(classOf[TAApplicationProvider])
+
+  /**
+    * Calculates a defaut taskadapter root folder. Current implementation
+    * returns <code>user.home / taskadapter</code>.
+    *
+    * @return task adapter root folder.
+    */
+  def getDefaultRootFolder(): File = {
+    val userHome = System.getProperty("user.home")
+    new File(userHome, ".taskadapter")
+  }
+
+  // Application config root folder.
+  val rootFolder: File = getDefaultRootFolder()
+  val credentialsStore = new FSCredentialStore(rootFolder)
+
+  /**
+    * Global credentials manager. TODO: It is not threadsafe yet, but should
+    * be.
+    */
+  private val credentialsManager = new BasicCredentialsManager(credentialsStore, 50)
+
+  /**
+    * Global services.
+    */
+  private val services = new Preservices(rootFolder, EditorManager.fromResource("editors.txt"))
+
+  services.licenseManager.loadInstalledTaskAdapterLicense()
+  log.info("Started TaskAdapter " + services.currentTaskAdapterVersion)
+
+  if (services.licenseManager.isSomeValidLicenseInstalled) {
+    val license = services.licenseManager.getLicense
+    log.info("License info: valid until " + license.getExpiresOn + ". Registered to " + license.getEmail)
+  } else {
+    log.info("License NOT installed or is NOT valid. Trial mode.")
+  }
+
+  /**
+    * Vaadin calls this when app is loaded in browser
+    */
+  override def createInstance(event: UICreateEvent): UI = {
+    val ui = new DummyUI
+    val tracker = if (reportGoogleAnalytics) {
+      val gaTracker = new GoogleAnalyticsTracker(GOOGLE_ANALYTICS_ID, "none")
+      gaTracker.extend(ui)
+      new GATrackerImpl(gaTracker)
+    } else {
+      log.info("Skipping Google Analytics: started in dev mode")
+      new NoOpGATracker()
+    }
+    SessionController.manageSession(services, credentialsManager, new WebUserSession(ui, tracker))
+    val action = if (services.licenseManager.isSomeValidLicenseInstalled) {
+      "web_app_opened_licensed"
+    } else {
+      "web_app_opened_trial"
+    }
+    tracker.trackEvent("general", action, services.currentTaskAdapterVersion)
+    ui
+  }
+
+  override def getUIClass(event: UIClassSelectionEvent): Class[_ <: UI] = classOf[DummyUI]
+}
