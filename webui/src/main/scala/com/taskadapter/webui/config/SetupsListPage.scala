b12c231b92ce9aab03c570a265017d0007e4628a
Alexey Skorokhodov <askorokhodov@twilio.com>
2017-08-14 18:12:16 -0700

    added "intro" label, improved design on "setups list" page

--- webui/src/main/scala/com/taskadapter/webui/config/SetupsListPage.scala
+++ webui/src/main/scala/com/taskadapter/webui/config/SetupsListPage.scala
@@ -4,6 +4,7 @@ import com.taskadapter.web.uiapi.SetupId
 import com.taskadapter.webui.{ConfigOperations, Page}
 import com.vaadin.server.Sizeable.Unit.PIXELS
 import com.vaadin.ui._
+import com.vaadin.ui.themes.ValoTheme
 
 class SetupsListPage(configOperations: ConfigOperations,
                      showEditSetup: (SetupId) => Unit,
@@ -12,10 +13,18 @@ class SetupsListPage(configOperations: ConfigOperations,
   layout.setSpacing(true)
   layout.setWidth(560, PIXELS)
 
-  private val addButton = new Button(Page.message("setupsListPage.addButton"))
+  val introLabel = new Label(Page.message("setupsListPage.intro"))
+  introLabel.addStyleName(ValoTheme.LABEL_H3)
+  val addButton = new Button(Page.message("setupsListPage.addButton"))
   addButton.addClickListener(_ => showNewSetup())
 
-  layout.addComponent(addButton)
+  private val introRow = new HorizontalLayout(introLabel, addButton)
+  introRow.setComponentAlignment(introLabel, Alignment.MIDDLE_LEFT)
+  introRow.setComponentAlignment(addButton, Alignment.MIDDLE_RIGHT)
+  introRow.setWidth("100%")
+  introRow.setExpandRatio(introLabel, 1)
+
+  layout.addComponent(introRow)
 
   val ui = layout
 
@@ -35,27 +44,27 @@ class SetupsListPage(configOperations: ConfigOperations,
     val setups = configOperations.getConnectorSetups()
     setups.sortBy(x => x.connectorId)
       .foreach { setup =>
-      val deleteButton = new Button(Page.message("setupsListPage.deleteButton"))
-      val editButton = new Button(Page.message("setupsListPage.editButton"))
-      val setupId = SetupId(setup.id.get)
-      deleteButton.addClickListener(_ => {
-        configOperations.deleteConnectorSetup(setupId)
-        refresh()
-      })
+        val deleteButton = new Button(Page.message("setupsListPage.deleteButton"))
+        val editButton = new Button(Page.message("setupsListPage.editButton"))
+        val setupId = SetupId(setup.id.get)
+        deleteButton.addClickListener(_ => {
+          configOperations.deleteConnectorSetup(setupId)
+          refresh()
+        })
 
-      editButton.addClickListener(_ => {
-        showEditSetup(setupId)
-      })
-      val connectorIdLabel = new Label(setup.connectorId)
-      connectorIdLabel.setData(setupId)
-      val setupLabel = new Label(setup.label)
-      setupLabel.setData(setupId)
-      grid.addComponents(connectorIdLabel,
-        setupLabel,
-        editButton,
-        deleteButton
-      )
-    }
+        editButton.addClickListener(_ => {
+          showEditSetup(setupId)
+        })
+        val connectorIdLabel = new Label(setup.connectorId)
+        connectorIdLabel.setData(setupId)
+        val setupLabel = new Label(setup.label)
+        setupLabel.setData(setupId)
+        grid.addComponents(connectorIdLabel,
+          setupLabel,
+          editButton,
+          deleteButton
+        )
+      }
   }
 
 }
