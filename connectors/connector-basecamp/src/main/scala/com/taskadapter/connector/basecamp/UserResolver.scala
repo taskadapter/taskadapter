497eafcd09e1db662a79e867afa72029fffa064c
Alexey Skorokhodov <alskor@gmail.com>
2017-08-26 08:16:21 -0700

    minor cleanup in Basecamp2

---
+++ connectors/connector-basecamp/src/main/scala/com/taskadapter/connector/basecamp/UserResolver.scala
@@ -0,0 +1,14 @@
+package com.taskadapter.connector.basecamp
+
+import com.taskadapter.model.GUser
+
+trait UserResolver {
+  /**
+    * Resolves a user by a user. Converts user into a Basecamp user. May return
+    * <code>null</code> if no user found. May receive null as input.
+    *
+    * @param user user to resolve.
+    * @return resolved user.
+    */
+    def resolveUser(user: GUser): GUser
+}
