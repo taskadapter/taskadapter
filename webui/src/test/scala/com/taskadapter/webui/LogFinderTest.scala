3d7e9e82d9db4230bd65092022a7dc9a856fddac
Alexey Skorokhodov <alskor@gmail.com>
2018-03-25 19:46:18 -0700

    more cleanup in LogFinderTest

--- webui/src/test/scala/com/taskadapter/webui/LogFinderTest.scala
+++ webui/src/test/scala/com/taskadapter/webui/LogFinderTest.scala
@@ -1,12 +1,11 @@
 package com.taskadapter.webui
 
-import com.taskadapter.webui.uiapi.ConfigsTempFolder
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FunSpec, Matchers}
 
 @RunWith(classOf[JUnitRunner])
-class LogFinderTest extends FunSpec with Matchers with ConfigsTempFolder {
+class LogFinderTest extends FunSpec with Matchers {
   it("finds the log file") {
     val location = LogFinder.getLogFileLocation()
     location should include("taskadapter.log")
