3b96053c203cc1cc95146c010dece612e66398ab
Alexey Skorokhodov <alskor@gmail.com>
2018-06-30 14:39:07 -0700

    catch exception if "last version" url cannot be loaded
    found this while domain name resolution was failing on my network.
    this exception was preventing the app from starting.

--- webui/src/main/scala/com/taskadapter/webui/LastVersionLoader.scala
+++ webui/src/main/scala/com/taskadapter/webui/LastVersionLoader.scala
@@ -2,15 +2,23 @@ package com.taskadapter.webui
 
 import com.taskadapter.connector.PropertiesUtf8Loader
 import com.taskadapter.http.HttpCaller
+import org.slf4j.LoggerFactory
 
 object LastVersionLoader {
+  private val log = LoggerFactory.getLogger("default")
+
   /**
     * check the last TA version available for download on the website.
     */
-    def loadLastVersion: String = {
-      val properties = PropertiesUtf8Loader.load("taskadapter.properties")
-      val url = properties.getProperty("update_site_url")
+  def loadLastVersion: String = {
+    val properties = PropertiesUtf8Loader.load("taskadapter.properties")
+    val url = properties.getProperty("update_site_url")
+    try {
       val lastVersionString = HttpCaller.get(url, classOf[String])
       lastVersionString.trim
+    } catch {
+      case e: Exception => log.error("Cannot load last version info: " + e.toString)
+        "unknown"
     }
+  }
 }
