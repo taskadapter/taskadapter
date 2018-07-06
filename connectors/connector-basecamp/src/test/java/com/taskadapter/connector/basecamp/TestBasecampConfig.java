487479740e1e979664732e84a6269c745d9280f0
Alexey Skorokhodov <alskor@gmail.com>
2018-07-05 18:04:17 -0700

    converted Basecamp 2 editor factory class from java to scala

--- connectors/connector-basecamp/src/test/java/com/taskadapter/connector/basecamp/TestBasecampConfig.java
+++ connectors/connector-basecamp/src/test/java/com/taskadapter/connector/basecamp/TestBasecampConfig.java
@@ -5,14 +5,14 @@ import com.taskadapter.connector.definition.WebConnectorSetup;
 import scala.Option;
 
 class TestBasecampConfig {
-    private static final String USER_ID = "4039521";
+    private static final String accountId = "4039521";
     private static final String USER_LOGIN = "d42dse+81ho6e4c19m44@sharklasers.com";
     private static final String USER_PASSWORD = "1234567f";
     private static final String PROJECT_KEY = "15524708";
 
     static BasecampConfig config() {
         BasecampConfig config = new BasecampConfig();
-        config.setAccountId(USER_ID);
+        config.setAccountId(accountId);
         config.setProjectKey(PROJECT_KEY);
         config.setFindUserByName(true);
         return config;
