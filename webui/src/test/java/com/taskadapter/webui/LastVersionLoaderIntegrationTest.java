02c6ea49b8f6596d27dc41ff186763c5d649f7aa
Alexey Skorokhodov <alskor@gmail.com>
2018-07-16 18:45:15 -0700

    re-enabled Last Version check test after upgrading JDK on the dev server

--- webui/src/test/java/com/taskadapter/webui/LastVersionLoaderIntegrationTest.java
+++ webui/src/test/java/com/taskadapter/webui/LastVersionLoaderIntegrationTest.java
@@ -12,7 +12,6 @@ public class LastVersionLoaderIntegrationTest {
      * stopped working after migrating TaskAdapter website from http://www.taskadapter.com to https://taskadapter.com.
      * disabling for today to be able to release a new version. this check works fine in the app itself.
      */
-    @Ignore
     @Test
     public void lastVersionIsLoadedFromUpdateServer() {
         String availableVersion = LastVersionLoader.loadLastVersion();
