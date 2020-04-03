4aae9eb7a9b1e4aead3ece8a121838067e651815
Al Sko <alskor@gmail.com>
2020-04-03 15:50:34 +0000

    * changed error reporting from GMail to Rollbar

--- webui/src/main/scala/com/taskadapter/reporting/ErrorReporter.scala
+++ webui/src/main/scala/com/taskadapter/reporting/ErrorReporter.scala
@@ -1,45 +1,44 @@
 package com.taskadapter.reporting
 
 import com.google.common.base.Throwables
+import com.rollbar.notifier.Rollbar
+import com.rollbar.notifier.config.ConfigBuilder
 import com.taskadapter.web.SettingsManager
 import com.taskadapter.web.uiapi.UISyncConfig
 import com.taskadapter.webui.results.ExportResultFormat
 import com.taskadapter.webui.service.CurrentVersionLoader
 
 object ErrorReporter {
-  private val version = new CurrentVersionLoader().getCurrentVersion
-  val defaultSubject = s"TaskAdapter error. $version"
+  private val appVersion = new CurrentVersionLoader().getCurrentVersion
+  private val rollbar = Rollbar.init(ConfigBuilder.withAccessToken("7443b08768344185beae9cfe6828dc81").build)
 
   def reportIfAllowed(config: UISyncConfig, throwable: Throwable): Unit = {
     if (isAllowedToSend) {
-      val mailBody = getConfigInfo(config) + div + Throwables.getStackTraceAsString(throwable)
-      sendMail(defaultSubject, mailBody)
+      val fullText = getHeader(config) + Throwables.getStackTraceAsString(throwable)
+      rollbar.error(fullText)
     }
   }
 
   def reportIfAllowed(throwable: Throwable): Unit = {
     if (isAllowedToSend) {
-      val mailBody = Throwables.getStackTraceAsString(throwable)
-      sendMail(defaultSubject + " - uncaught exception", mailBody)
+      rollbar.error(throwable)
     }
   }
 
   def reportIfAllowed(config: UISyncConfig, results: ExportResultFormat): Unit = {
     if (isAllowedToSend && results.hasErrors) {
-      val mailBody = getConfigInfo(config) + div + ExportResultsFormatter.toNiceString(results)
-      val subject = s"TaskAdapter export error. $version. Config '${config.label}'"
-      sendMail(subject, mailBody)
+      val mailBody = getHeader(config) + ExportResultsFormatter.toNiceString(results)
+      rollbar.log(mailBody)
     }
+  }
+
+  private def getHeader(config: UISyncConfig): String = {
+    getConfigInfo(config) + s"$div TaskAdapter v. $appVersion $div"
   }
 
   private def getConfigInfo(config: UISyncConfig): String = {
     config.getConnector1.getConnectorTypeId + " - " + config.getConnector2.getConnectorTypeId +
       div + FieldMappingFormatter.format(config.fieldMappings)
-  }
-
-  private def sendMail(subject: String, mailBody: String): Unit = {
-    MailSender.sendFromGMail(MailSettings.fromEmail,
-      MailSettings.fromPassword, MailSettings.toEmail, subject, mailBody)
   }
 
   def isAllowedToSend: Boolean = new SettingsManager().isErrorReportingEnabled
