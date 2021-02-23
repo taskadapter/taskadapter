ae01728b83f0ef4bb6e0aa886a8268a16b78b369
Alexey Skorokhodov <alskor@gmail.com>
2021-02-22 19:32:10 -0800

    show the number of loaded tasks in Export Confirmation dialog

--- webui/src/main/scala/com/taskadapter/webui/export/ConfirmExportFragment.scala
+++ webui/src/main/scala/com/taskadapter/webui/export/ConfirmExportFragment.scala
@@ -1,9 +1,9 @@
 package com.taskadapter.webui.export
 
 import java.util
-
 import com.taskadapter.config.StorageException
 import com.taskadapter.model.GTask
+import com.taskadapter.web.ui.HtmlLabel
 import com.taskadapter.web.uiapi.UISyncConfig
 import com.taskadapter.webui.{ConfigOperations, Page}
 import com.taskadapter.webui.action.MyTree
@@ -13,6 +13,7 @@ import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.html.Label
 import com.vaadin.flow.component.orderedlayout.{HorizontalLayout, VerticalLayout}
 import org.slf4j.LoggerFactory
+
 import scala.collection.JavaConverters._
 
 object ConfirmExportFragment {
@@ -46,10 +47,18 @@ object ConfirmExportFragment {
     val resolver = config.getPreviouslyCreatedTasksResolver()
     val layout = new VerticalLayout
     layout.setSpacing(true)
+
+    val loadedTasksLabel = new HtmlLabel(Page.message("exportConfirmation.loadedTasks",
+      initialTasks.size() + "",
+      config.getConnector1.getLabel,
+      config.getConnector1.getSourceLocation))
     val destinationLocation = config.getConnector2.getDestinationLocation
     val destinationWithDecoration = destinationLocation + " (" + config.getConnector2.getConnectorTypeId + ")"
     val text1 = new Label(Page.message("exportConfirmation.pleaseConfirm", destinationWithDecoration))
-    layout.add(text1)
+
+    layout.add(loadedTasksLabel,
+      text1)
+
     val connectorTree = new MyTree(resolver, initialTasks, destinationLocation)
     layout.add(connectorTree.getTree)
     val buttonsLayout = new HorizontalLayout
