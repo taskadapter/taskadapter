a579c6a4bc1e8f1f7434ace640d5bb914cb84a73
Alexey Skorokhodov <alskor@gmail.com>
2021-07-14 08:53:06 -0700

    marked LastVersionLoaderIntegrationTest.java with the new IntegrationTest annotation

--- webui/src/test/java/com/taskadapter/webui/LastVersionLoaderIntegrationTest.java
+++ webui/src/test/java/com/taskadapter/webui/LastVersionLoaderIntegrationTest.java
@@ -1,11 +1,14 @@
 package com.taskadapter.webui;
 
+import com.taskadapter.test.core.IntegrationTest;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@Category(IntegrationTest.class)
 public class LastVersionLoaderIntegrationTest {
 
     /**
